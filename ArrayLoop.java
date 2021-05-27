43690ad973ef6332221eacd73ee32c983b8768f4
nerd291 <thenerd291@gmail.com>
2021-05-27 18:02:26 -0400

    add arrayloop

---
+++ ArrayLoop.java
@@ -0,0 +1,11 @@
+public class ArrayLoop {
+    public static void main(String args[]){
+        int[] squareNumbers = new int[10];
+
+        for(int i=0;i<10;++i){
+            int square = (i+1)*(i+1);
+            squareNumbers[i]=square;
+            System.out.println(squareNumbers[i]);
+        }
+    }
+}
---
+++ DVD.java
@@ -0,0 +1,19 @@
+public class DVD {
+
+    public DVD[] dvdCollection = new DVD[15];
+
+    public String name;
+    public int releaseYear;
+    public String director;
+
+    public DVD(String name, int releaseYear, String director) {
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.director = director;
+    }
+
+    // public String toString() {
+    //     System.out.println(
+    //         this.name + ", directed by " + this.director + ", released in " + this.releaseYear);
+    // }
+}
